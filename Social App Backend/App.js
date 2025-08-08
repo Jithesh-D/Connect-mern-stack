6151f0ad71a09b6938bca6f5918da0d1a6f849fe
JITHESH.D <jitheshd0006@gmail.com>
2025-08-08 20:16:42 +0530

    Update App.js

---
+++ Social App Backend/App.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3001;
+app.use(express.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});

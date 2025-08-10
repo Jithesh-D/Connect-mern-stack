d9c35162e49b71832cba45d36b5d1d2fffd2bf7c
Jithesh D <jitheshd0006@gmail.com>
2025-08-10 18:44:59 +0530

    Updated components,backend folders and added service folder

--- Social App Backend/App.js
+++ Social App Backend/App.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const PORT = 3001;
+
 app.use(express.urlencoded({ extended: true }));
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
---
+++ Social App Frontend/src/services/service.jsx
@@ -0,0 +1,35 @@
+const addPostToServer = async (userId, title, body, tags, reactions) => {
+  const response = await fetch("http://localhost:3000/api/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userId, title, body, tags, reactions }),
+  });
+  const post = await response.json();
+  return mapPostFromServer(post);
+};
+
+const getPostsFromServer = async () => {
+  const response = await fetch("http://localhost:3000/api/posts");
+  const posts = await response.json();
+  return posts.map(mapPostFromServer);
+};
+const deletePostFromServer = async (postId) => {
+  await fetch(`http://localhost:3000/api/posts/${postId}`, {
+    method: "DELETE",
+  });
+};
+
+const mapPostFromServer = (post) => {
+  return {
+    id: post.id,
+    userId: post.userId,
+    title: post.title,
+    body: post.body,
+    tags: post.tags,
+    reactions: post.reactions,
+  };
+};
+
+export { addPostToServer, getPostsFromServer, deletePostFromServer };

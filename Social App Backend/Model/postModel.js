9a7989aaf1b503dc3452f39a520765cc4823d015
JITHESH.D <jitheshd0006@gmail.com>
2025-08-14 19:34:32 +0530

    Update postModel.js

--- Social App Backend/Model/postModel.js
+++ Social App Backend/Model/postModel.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
   title: { type: String, required: true },
   body: { type: String, required: true },
   tags: { type: [String], required: true },
